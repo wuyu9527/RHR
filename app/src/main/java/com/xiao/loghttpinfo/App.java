f61977b9a964ea8b63e7c8c20ece671bb8fcd392
wuhx <546446588@qq.com>
2018-07-21 12:29:37 +0800

    RHR使用

--- app/src/main/java/com/xiao/loghttpinfo/App.java
+++ app/src/main/java/com/xiao/loghttpinfo/App.java
@@ -2,6 +2,11 @@ package com.xiao.loghttpinfo;
 
 import android.app.Application;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+
 /**
  * Created by XiaoJianjun on 2017/1/16.
  */
@@ -9,7 +14,26 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        // 初始化Looger工具
-        LogUtil.init(BuildConfig.LOG_DEBUG);
+        setLoggerInit();
+    }
+
+    /**
+     * 调试日志打印
+     * 初始化Looger工具
+     */
+    private void setLoggerInit(){
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(true)  // 是否打印线程号,默认true
+                .methodCount(5)         // 展示几个方法数,默认2
+                .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
+//                .logStrategy(customLog) //是否更换打印输出,默认为logcat
+                .tag("doctor")   // 全局的tag
+                .build();
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy){
+            @Override
+            public boolean isLoggable(int priority, String tag) {
+                return BuildConfig.DEBUG;
+            }
+        });
     }
 }
